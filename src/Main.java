6d46b6fc3ea41d426f4352d43c940ff3dc97a12b
tKons64 <spb_unex@mail.ru>
2022-09-06 19:45:59 +0300

    Практические задачи, урок №6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {task1();task2();}
+
+    public static void task1(){
         System.out.println("Hello world!");
+        }
+
+    public static void task2(){
+        System.out.println("Hello world!!!!");
     }
+
+
 }
