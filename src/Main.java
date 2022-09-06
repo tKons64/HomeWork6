1858d64bf95c1ccbcf4cb2b533d7e5deab7ea436
tKons64 <spb_unex@mail.ru>
2022-09-06 20:01:38 +0300

    Выполненны задачи №1 - №4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
 public class Main {
-    public static void main(String[] args) {task1();task2();}
+    public static void main(String[] args) {task1();task2();task3();task4();}
 
-    public static void task1(){
-        System.out.println("Hello world!");
+    public static void task1() {
+        System.out.println("");
+        System.out.println("Задание №1");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Итерация цикла: " + i);
         }
-
-    public static void task2(){
-        System.out.println("Hello world!!!!");
     }
 
+    public static void task2(){
+        System.out.println("");
+        System.out.println("Задание №2");
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Итерация цикла: " + i);
+        }
+    }
+
+    public static void task3(){
+        System.out.println("");
+        System.out.println("Задание №3");
+
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0 && i != 0){
+                System.out.println("Это четное число - " + i);
+            }
+        }
+    }
+
+    public static void task4(){
+        System.out.println("");
+        System.out.println("Задание №4");
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println("Итерация цикла: " + i);
+        }
+    }
 
 }
